6a8cdddc49d50864acbf771dd14ffcb7a94b2837
chokyoungjin <kyoungjin@kylie-13.local>
2025-03-18 10:29:16 +0900

    initial commit

---
+++ src/test/java/__kylie_cho/community_be/CommunityBeApplicationTests.java
@@ -0,0 +1,13 @@
+package __kylie_cho.community_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
