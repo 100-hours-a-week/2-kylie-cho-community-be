e695fde04a01d6eb6f5584a5df3577dc40226786
chokyoungjin <kyoungjin@kylie-13.local>
2025-03-26 21:46:26 +0900

    FE 연동: 게시글 생성 기능 완료

--- src/main/java/__kylie_cho/community_be/dto/PostResponseDto.java
+++ src/main/java/__kylie_cho/community_be/dto/PostResponseDto.java
@@ -9,18 +9,19 @@ public class PostResponseDto {
     private Long id;
     private String title;
     private String content;
+    private String image;
     private int heartCount;
     private int viewCount;
     private int commentCount;
     private String createdAt;
     private String updatedAt;
-    private String image;
     private UserSummaryDto user;
 
     public PostResponseDto(Post post, long viewCount, long commentCount, long heartCount) {
         this.id = post.getId();
         this.title = post.getTitle();
         this.content = post.getContent();
+        this.image = post.getImage();
         this.heartCount = (int) heartCount;
         this.viewCount = (int) viewCount;
         this.commentCount = (int) commentCount;
